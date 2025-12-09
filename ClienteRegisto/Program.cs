98fea234af7b837813d081c601868a2e7c4b9c9c
Rui Pedro <ruipedro@MacBook-Pro-De-Rui-Pedro.local>
2025-12-09 21:11:46 +0000

    Aplicação cliente para registo

---
+++ ClienteRegisto/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Grpc.Net.Client;
+using VotingSystem; 
+
+//Permissão de HTTP2 sem TLS (desenvolvimento local no mac)
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+
+var channel = GrpcChannel.ForAddress("http://localhost:5137");
+var client = new VoterRegistrationService.VoterRegistrationServiceClient(channel);
+
+Console.WriteLine("=== Registo (Cliente) ===");
+Console.Write("Insira o Nº Cartão Cidadão: ");
+var cc = Console.ReadLine();
+
+try {
+    var reply = await client.IssueVotingCredentialAsync(new VoterRequest { CitizenCardNumber = cc });
+    
+    if (reply.IsEligible) {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"\nSucesso! Credencial emitida: {reply.VotingCredential}");
+        Console.WriteLine("Reserve esta credencial para utilizar na App de Votação.");
+    } else {
+        Console.WriteLine("Cidadão não elegível.");
+    }
+} catch (Exception ex) {
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+Console.ResetColor();
